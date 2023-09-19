b39ee60f9a01365a20c1950de81d636a10fe9c60
Radomir <114557710+RadomirSz@users.noreply.github.com>
2023-09-19 16:32:50 +0100

    start

---
+++ javasc.js
@@ -0,0 +1,17 @@
+function test(){
+alert("witam");
+}
+
+function mnozenie()
+{
+    a = prompt("podaj liczbe","");
+    b = prompt("podaj liczbe","");
+    alert("pomnożone dają:" + a*b);
+}
+
+function DIVIDE()
+{
+    a = prompt("podaj liczbe","");
+    b = prompt("podaj liczbe","");
+    alert("pomnożone dają:" + a/b);
+}
