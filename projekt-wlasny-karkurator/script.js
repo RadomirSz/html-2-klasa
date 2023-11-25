1fb1762456cdf14da84be9b17fd0be6d0c0f889e
RadomirSz <r.szpruta@gmail.com>
2023-11-25 01:46:07 +0100

    cummit coś pozmieniałem coś usunąłem coś nie działa w ogóle, coś działa lepiej

--- projekt-wlasny-karkurator/script.js
+++ projekt-wlasny-karkurator/script.js
@@ -4,6 +4,7 @@ function add(x)
 {
     dzialanie += document.getElementById(x).value;
     document.getElementById("ekran").innerHTML = dzialanie;
+    console.log("dodało");
 }
 
 function wynik()
@@ -11,22 +12,30 @@ function wynik()
     if(dzialanie == "")
     {
         document.getElementById("ekran").innerHTML = "nic nie dałeś baranie";
+        console.log("pustka");
     }
     else
     {
         document.getElementById("ekran").innerHTML = eval(dzialanie);
         dzialanie = eval(dzialanie);
+
     }
     
 }
 
-function clear(){ 
-    dzialanie = "";
-    document.getElementById("ekran").innerHTML = "";
-}
+// function clear(){ 
+//     dzialanie = "";
+//     document.getElementById("ekran").innerHTML = "";
+// }
 
 function del(){
-    dzialanie = dzialanie.substring(0, dzialanie.length - 1); 
-    document.getElementById("ekran").innerHTML = dzialanie;
-
+    dzialanie = dzialanie - dzialanie[dzialanie.length];
+    if(dzialanie.length > 0){
+        document.getElementById("ekran").innerHTML = dzialanie;
+    }
+    else{
+        document.getElementById("ekran").innerHTML = "";
+        dzialanie = "";
+        console.log("jakiś els");
+    }
 }
