0dd51e9fa8b49c03f3f27b964b96c6d9b861c8ba
RadomirSz <r.szpruta@gmail.com>
2023-11-27 18:49:58 +0100

    cos jeszcze skrocilem

--- projekt-wlasny-karkurator/script.js
+++ projekt-wlasny-karkurator/script.js
@@ -5,18 +5,13 @@ function add(x)
     dzialanie += document.getElementById(x).value;
     document.getElementById("ekran").innerHTML = dzialanie;
     console.log("dodało");
-    if(dzialanie.slice(dzialanie.length - 1 , dzialanie.length) && x == 0)
-    {
-       document.getElementById("ekran").innerHTML = "nie dziel przez zero";
-       
-    }
 }
 
 function wynik()
 {
     if(dzialanie == "")
     {
-        document.getElementById("ekran").innerHTML = "nic nie dałeś baranie";
+        document.getElementById("ekran").innerHTML = "nic nie dałeś";
         console.log("pustka");
     }
     else
@@ -34,17 +29,7 @@ function czysc(){
 }
 
 function del(){
-    if(dzialanie.length == 1)
-    {
-        dzialanie = "";
-    }
-
-    dzialanie = dzialanie.slice(0,dzialanie.length - 1);
-    if(dzialanie.length > 0){
-        document.getElementById("ekran").innerHTML = dzialanie;
-    }
-    else{
-        document.getElementById("ekran").innerHTML = "";
-        dzialanie = "";
-        }
+    if(dzialanie.length == 1) dzialanie = "";
+    else dzialanie = dzialanie.slice(0,dzialanie.length - 1);
+    document.getElementById("ekran").innerHTML = dzialanie;
 }
